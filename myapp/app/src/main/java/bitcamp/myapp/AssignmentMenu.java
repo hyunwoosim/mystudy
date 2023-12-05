32f128a622f73a2fbc0b644a5b13f77098c61524
hyunwoosim <kshsim98@naver.com>
2023-12-05 20:45:21 +0900

    12.05복습

--- myapp/app/src/main/java/bitcamp/myapp/AssignmentMenu.java
+++ myapp/app/src/main/java/bitcamp/myapp/AssignmentMenu.java
@@ -2,24 +2,34 @@ package bitcamp.myapp;
 
 public class AssignmentMenu {
 
+    static void printMenu() {
+        System.out.println("[과제]");
+        System.out.println("1. 등록");
+        System.out.println("2. 조회");
+        System.out.println("3. 변경");
+        System.out.println("4. 삭제");
+        System.out.println("0. 이전");
+
+    }
+
     static void execute() {
         printMenu();
-
+        Amd a1 = new Amd();
         while (true) {
-            String input = Prompt.input("메인/과제");
+            String input = Prompt.input("메인/과제> ");
 
             switch (input) {
                 case "1":
-                    System.out.println("등록입니다.");
+                    add();
                     break;
                 case "2":
-                    System.out.println("조회입니다.");
+                    view();
                     break;
                 case "3":
-                    System.out.println("변경입니다.");
+                    modify(a1);
                     break;
                 case "4":
-                    System.out.println("삭제입니다.");
+                    delete();
                     break;
                 case "0":
                     return;
@@ -32,13 +42,33 @@ public class AssignmentMenu {
         }
     }
 
-    static void printMenu() {
-        System.out.println("[과제]");
-        System.out.println("1. 등록");
-        System.out.println("2. 조회");
-        System.out.println("3. 변경");
-        System.out.println("4. 삭제");
-        System.out.println("0. 이전");
+    static void add(Amd a1) {
 
+        System.out.println("과제 등록:");
+        a1.title = Prompt.input("과제명? ");
+        a1.content = Prompt.input("내용? ");
+        a1.deadline = Prompt.input("제출 마감일? ");
     }
+
+    static void view(Amd a1) {
+        System.out.println("과제 조회:");
+        System.out.printf("과제명: %s\n", a1.title);
+        System.out.printf("내용: %s\n", a1.content);
+        System.out.printf("제출 마감일: %s\n", a1.deadline);
+    }
+
+    static void modify(Amd a1) {
+        System.out.println("과제 변경:");
+        a1.title = Prompt.input("과제명(%s)? ", a1.title);
+        a1.content = Prompt.input("내용(%s)? ", a1.content);
+        a1.deadline = Prompt.input("제출 마감일(%s)? ", a1.deadline);
+    }
+
+    static void delete() {
+        System.out.println("과제 삭제:");
+        Amd.title = "";
+        Amd.content = "";
+        Amd.deadline = "";
+    }
+
 }
--- myapp/app/src/main/java/bitcamp/myapp/BoardMenu.java
+++ myapp/app/src/main/java/bitcamp/myapp/BoardMenu.java
@@ -2,25 +2,41 @@ package bitcamp.myapp;
 
 public class BoardMenu {
 
+    static String title;
+    static String content;
+    static String wirter;
+    static String date;
+
+
+    static void printMenu() {
+        System.out.println("[게시글]");
+        System.out.println("1. 등록");
+        System.out.println("2. 조회");
+        System.out.println("3. 변경");
+        System.out.println("4. 삭제");
+        System.out.println("0. 이전");
+    }
+
+
     static void execute() {
         printMenu();
 
         loop2:
         while (true) {
-            String input = Prompt.input("메인/게시글");
+            String input = Prompt.input("메인/게시글> ");
 
             switch (input) {
                 case "1":
-                    System.out.println("등록입니다.");
+                    add();
                     break;
                 case "2":
-                    System.out.println("조회입니다.");
+                    content();
                     break;
                 case "3":
-                    System.out.println("변경입니다.");
+                    writer();
                     break;
                 case "4":
-                    System.out.println("삭제입니다.");
+                    date();
                     break;
                 case "0":
                     break loop2;
@@ -33,12 +49,38 @@ public class BoardMenu {
         }
     }
 
-    static void printMenu() {
-        System.out.println("[게시글]");
-        System.out.println("1. 등록");
-        System.out.println("2. 조회");
-        System.out.println("3. 변경");
-        System.out.println("4. 삭제");
-        System.out.println("0. 이전");
+    static void add() {
+        System.out.println("게시글 제목: ");
+        title = Prompt.input("제목? ");
+        content = Prompt.input("내용? ");
+        wirter = Prompt.input("작성자? ");
+        date = Prompt.input("작성일 ");
+
+    }
+
+    static void content() {
+        System.out.println("게시글 조회: ");
+        System.out.printf("제목 %s\n", title);
+        System.out.printf("내용 %s\n", content);
+        System.out.printf("작성자 %s\n", wirter);
+        System.out.printf("작성일 %s\n", date);
+
+    }
+
+    static void writer() {
+        System.out.println("게시글 변경: ");
+        title = Prompt.input("제목(%s)?", title);
+        content = Prompt.input("내용(%s)?", content);
+        wirter = Prompt.input("작성자(%s)?", wirter);
+        date = Prompt.input("작성일(%s)?", date);
+    }
+
+    static void date() {
+        System.out.println("게시글 삭제: ");
+        title = "";
+        content = "";
+        wirter = "";
+        date = "";
+
     }
 }
