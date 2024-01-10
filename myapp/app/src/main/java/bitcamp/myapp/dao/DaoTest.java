e415777874579695b6341f6ad19a5193acad0e71
hyunwoosim <kshsim98@naver.com>
2024-01-10 20:15:52 +0900

    24.01.10

---
+++ myapp/app/src/main/java/bitcamp/myapp/dao/DaoTest.java
@@ -0,0 +1,17 @@
+package bitcamp.myapp.dao;
+
+import bitcamp.myapp.vo.Board;
+
+public class DaoTest extends AbstractDao<Board> {
+
+
+    public static void main(String[] args) {
+        DaoTest obj = new DaoTest();
+        obj.loadData("app/board.json");
+
+        for (Board board : obj.list) {
+            System.out.println(board);
+        }
+
+    }
+}
