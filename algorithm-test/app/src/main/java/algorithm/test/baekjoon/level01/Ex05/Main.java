6243bf7e12b669b602d5ea3109584abb65734559
hyunwoosim <kshsim98@naver.com>
2024-01-16 20:28:25 +0900

    24.01.16

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level01/Ex05/Main.java
@@ -0,0 +1,13 @@
+package algorithm.test.baekjoon.level01.Ex05;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Double a, b;
+    a = sc.nextDouble();
+    b = sc.nextDouble();
+    System.out.println(a / b);
+  }
+}
