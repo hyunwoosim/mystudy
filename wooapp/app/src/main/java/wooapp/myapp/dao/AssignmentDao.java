de0a8b916f0e7ab5bec2ee5b06b37c27abdf4e3f
hyunwoosim <kshsim98@naver.com>
2024-02-02 17:47:50 +0900

    24.02.02

---
+++ wooapp/app/src/main/java/wooapp/myapp/dao/AssignmentDao.java
@@ -0,0 +1,18 @@
+package wooapp.myapp.dao;
+
+import wooapp.myapp.vo.Assignment;
+import java.util.List;
+
+public interface AssignmentDao {
+
+  void add(Assignment assignment);
+
+  int delete(int no);
+
+  List<Assignment> findAll();
+
+  Assignment findBy(int no);
+
+  int update(Assignment assignment);
+
+}
---
+++ wooapp/app/src/main/java/wooapp/myapp/handler/assignment/AssignmentDeleteHandler.java
@@ -0,0 +1,30 @@
+package wooapp.myapp.handler.assignment;
+
+import wooapp.menu.AbstractMenuHandler;
+import wooapp.myapp.dao.AssignmentDao;
+import wooapp.util.Prompt;
+
+public class AssignmentDeleteHandler extends AbstractMenuHandler {
+
+  private AssignmentDao assignmentDao;
+
+  public AssignmentDeleteHandler(AssignmentDao assignmentDao, Prompt prompt) {
+    super(prompt);
+    this.assignmentDao = assignmentDao;
+  }
+
+  @Override
+  protected void action() {
+    try {
+      int no = this.prompt.inputInt("번호? ");
+      if (assignmentDao.delete(no) == 0) {
+        System.out.println("과제 번호가 유효하지 않습니다!");
+      } else {
+        System.out.println("과제를 삭제했습니다.");
+      }
+
+    } catch (Exception e) {
+      System.out.println("삭제 오류!");
+    }
+  }
+}
