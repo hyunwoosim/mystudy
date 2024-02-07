364a2f2578e0a3c5e292c6523f2f082d7d727c61
hyunwoosim <kshsim98@naver.com>
2024-02-07 16:44:06 +0900

    .

--- myapp/app-client/src/main/java/bitcamp/myapp/dao/mysql/MemberDaoImpl.java
+++ myapp/app-client/src/main/java/bitcamp/myapp/dao/mysql/MemberDaoImpl.java
@@ -4,99 +4,113 @@ import bitcamp.myapp.dao.DaoException;
 import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.vo.Member;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MemberDaoImpl implements MemberDao {
 
-  Connection con;
+    Connection con;
 
-  public MemberDaoImpl(Connection con) {
-    this.con = con;
-  }
-
-  @Override
-  public void add(Member member) {
-    try {
-      Statement stmt = con.createStatement();
-      stmt.executeUpdate(String.format(
-          "insert into members(email,name,password) values('%s','%s',sha2('%s',256))",
-          member.getEmail(), member.getName(), member.getPassword()));
-
-    } catch (Exception e) {
-      throw new DaoException("데이터 입력 오류", e);
+    public MemberDaoImpl(Connection con) {
+        this.con = con;
     }
-  }
 
-  @Override
-  public int delete(int no) {
-    try {
-      Statement stmt = con.createStatement();
-      return stmt.executeUpdate(String.format("delete from members where member_no=%d", no));
+    @Override
+    public void add(Member member) {
+        try (PreparedStatement pstmt = con.prepareStatement(
+            "insert into members(email,name,password) values(?,?,sha2(?,256))")) {
 
-    } catch (Exception e) {
-      throw new DaoException("데이터 삭제 오류", e);
+            pstmt.setString(1, member.getEmail());
+            pstmt.setString(1, member.getName());
+            pstmt.setString(1, member.getPassword());
+
+            pstmt.executeUpdate();
+
+        } catch (Exception e) {
+            throw new DaoException("데이터 입력 오류", e);
+        }
     }
-  }
 
-  @Override
-  public List<Member> findAll() {
-    try {
-      Statement stmt = con.createStatement();
-      ResultSet rs = stmt.executeQuery("select * from members");
+    @Override
+    public int delete(int no) {
+        try (PreparedStatement pstmt = con.prepareStatement(
+            "delete from members where member_no=?")) {
 
-      ArrayList<Member> list = new ArrayList<>();
+            pstmt.setInt(1, no);
 
-      while (rs.next()) {
-        Member member = new Member();
-        member.setNo(rs.getInt("member_no"));
-        member.setEmail(rs.getString("email"));
-        member.setName(rs.getString("name"));
-        member.setCreatedDate(rs.getDate("created_date"));
+            return pstmt.executeUpdate();
 
-        list.add(member);
-      }
-      return list;
-
-    } catch (Exception e) {
-      throw new DaoException("데이터 가져오기 오류", e);
+        } catch (Exception e) {
+            throw new DaoException("데이터 삭제 오류", e);
+        }
     }
-  }
 
-  @Override
-  public Member findBy(int no) {
-    try {
-      Statement stmt = con.createStatement();
-      ResultSet rs = stmt.executeQuery("select * from members where member_no = " + no);
+    @Override
+    public List<Member> findAll() {
+        try (PreparedStatement pstmt = con.prepareStatement(
+            "select member_no, name, email, created_date from members order by member_no desc")) {
 
-      if (rs.next()) {
-        Member member = new Member();
-        member.setNo(rs.getInt("member_no"));
-        member.setEmail(rs.getString("email"));
-        member.setName(rs.getString("name"));
-        member.setCreatedDate(rs.getDate("created_date"));
+            ResultSet rs = pstmt.executeQuery();
 
-        return member;
-      }
-      return null;
+            ArrayList<Member> list = new ArrayList<>();
 
-    } catch (Exception e) {
-      throw new DaoException("데이터 가져오기 오류", e);
+            while (rs.next()) {
+                Member member = new Member();
+                member.setNo(rs.getInt("member_no"));
+                member.setName(rs.getString("name"));
+                member.setEmail(rs.getString("email"));
+                member.setCreatedDate(rs.getDate("created_date"));
+
+                list.add(member);
+            }
+            return list;
+
+        } catch (Exception e) {
+            throw new DaoException("데이터 가져오기 오류", e);
+        }
     }
-  }
 
-  @Override
-  public int update(Member member) {
-    try {
-      Statement stmt = con.createStatement();
-      return stmt.executeUpdate(String.format(
-          "update members set email='%s', name='%s', password=sha2('%s',256) where member_no=%d",
-          member.getEmail(), member.getName(), member.getPassword(), member.getNo()));
+    @Override
+    public Member findBy(int no) {
+        try (PreparedStatement pstmt = con.prepareStatement(
+            "select * from members where member_no =?")) {
 
-    } catch (Exception e) {
-      throw new DaoException("데이터 변경 오류", e);
+            pstmt.setInt(1, no);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+
+                if (rs.next()) {
+                    Member member = new Member();
+                    member.setNo(rs.getInt("member_no"));
+                    member.setEmail(rs.getString("email"));
+                    member.setName(rs.getString("name"));
+                    member.setCreatedDate(rs.getDate("created_date"));
+
+                    return member;
+                }
+                return null;
+            }
+        } catch (Exception e) {
+            throw new DaoException("데이터 가져오기 오류", e);
+        }
     }
-  }
+
+    @Override
+    public int update(Member member) {
+        try (PreparedStatement pstmt = con.prepareStatement(
+            "update members set name=?, email=?, password=sha2(?,256) where member_no=?"
+        )) {
+            pstmt.setString(1, member.getName());
+            pstmt.setString(2, member.getEmail());
+            pstmt.setString(3, member.getPassword());
+            pstmt.setInt(4, member.getNo());
+
+            return pstmt.executeUpdate();
+
+        } catch (Exception e) {
+            throw new DaoException("데이터 변경 오류", e);
+        }
+    }
 }
