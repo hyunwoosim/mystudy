56bdcc45176965cf79a8d0ec6b77f0c46827d359
hyunwoosim <kshsim98@naver.com>
2024-02-08 20:41:23 +0900

    24.02.08

---
+++ myapp/app-server/src/main/java/bitcamp/menu/Menu.java
@@ -0,0 +1,11 @@
+package bitcamp.menu;
+
+import bitcamp.util.Prompt;
+
+
+public interface Menu {
+
+    void execute(Prompt prompt) throws Exception;
+
+    String getTitle();
+}
---
+++ myapp/app-server/src/main/java/bitcamp/menu/MenuHandler.java
@@ -0,0 +1,9 @@
+package bitcamp.menu;
+
+import bitcamp.util.Prompt;
+
+public interface MenuHandler {
+    
+
+    void action(Menu menu, Prompt prompt);
+}
