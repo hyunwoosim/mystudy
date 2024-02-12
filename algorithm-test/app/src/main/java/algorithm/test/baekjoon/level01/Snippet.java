b4c633d8661694a883944f3644c9662bfbdaa397
hyunwoosim <kshsim98@naver.com>
2024-02-12 18:01:37 +0900

    2024.02.12

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level01/Snippet.java
@@ -0,0 +1,18 @@
+package algorithm.test.baekjoon.level01;
+
+public class Snippet {
+   int prod = 1;
+  	    int x = 0;
+  
+          while(true) {
+              // prod가 n이 될 때까지 2를 곱합니다.
+              if(n == prod)
+                  break;
+              
+              prod *= 2;
+              x++;
+          }
+  
+          System.out.println(x);
+}
+
