e18b613087e9b1c775d1bf65bc784399bebeff32
hyunwoosim <kshsim98@naver.com>
2024-02-13 21:04:07 +0900

    24.02.13

--- myapp/app-server/src/main/java/bitcamp/myapp/dao/mysql/AssignmentDaoImpl.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/dao/mysql/AssignmentDaoImpl.java
@@ -3,6 +3,7 @@ package bitcamp.myapp.dao.mysql;
 import bitcamp.myapp.dao.AssignmentDao;
 import bitcamp.myapp.dao.DaoException;
 import bitcamp.myapp.vo.Assignment;
+import bitcamp.util.DBConnectionPool;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,103 +12,127 @@ import java.util.List;
 
 public class AssignmentDaoImpl implements AssignmentDao {
 
-  Connection con;
+    DBConnectionPool connectionPool;
 
-  public AssignmentDaoImpl(Connection con) {
-    this.con = con;
-  }
-
-  @Override
-  public void add(Assignment assignment) {
-    try (PreparedStatement pstmt = con.prepareStatement(
-        "insert into assignments(title,content,deadline) values(?,?,?)")) {
-
-      pstmt.setString(1, assignment.getTitle());
-      pstmt.setString(2, assignment.getContent());
-      pstmt.setDate(3, assignment.getDeadline());
-
-      pstmt.executeUpdate();
-
-    } catch (Exception e) {
-      throw new DaoException("데이터 입력 오류", e);
+    public AssignmentDaoImpl(DBConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
     }
-  }
 
-  @Override
-  public int delete(int no) {
-    try (PreparedStatement pstmt = con.prepareStatement(
-        "delete from assignments where assignment_no=?")) {
-      pstmt.setInt(1, no);
+    @Override
+    public void add(Assignment assignment) {
+        Connection con = null;
+        try {
+            con = connectionPool.getConnection();
 
-      return pstmt.executeUpdate();
+            try (PreparedStatement pstmt = con.prepareStatement(
+                "insert into assignments(title,content,deadline) values(?,?,?)")) {
 
-    } catch (Exception e) {
-      throw new DaoException("데이터 삭제 오류", e);
-    }
-  }
+                pstmt.setString(1, assignment.getTitle());
+                pstmt.setString(2, assignment.getContent());
+                pstmt.setDate(3, assignment.getDeadline());
 
-  @Override
-  public List<Assignment> findAll() {
-    try (PreparedStatement pstmt = con.prepareStatement(
-        "select assignment_no, title, deadline from assignments order by assignment_no desc");
-        ResultSet rs = pstmt.executeQuery()) {
+                pstmt.executeUpdate();
+            }
 
-      ArrayList<Assignment> list = new ArrayList<>();
-
-      while (rs.next()) {
-        Assignment assignment = new Assignment();
-        assignment.setNo(rs.getInt("assignment_no"));
-        assignment.setTitle(rs.getString("title"));
-        assignment.setDeadline(rs.getDate("deadline"));
-
-        list.add(assignment);
-      }
-      return list;
-
-    } catch (Exception e) {
-      throw new DaoException("데이터 가져오기 오류", e);
-    }
-  }
-
-  @Override
-  public Assignment findBy(int no) {
-    try (PreparedStatement pstmt = con.prepareStatement(
-        "select * from assignments where assignment_no=?")) {
-
-      pstmt.setInt(1, no);
-
-      try (ResultSet rs = pstmt.executeQuery()) {
-
-        if (rs.next()) {
-          Assignment assignment = new Assignment();
-          assignment.setNo(rs.getInt("assignment_no"));
-          assignment.setTitle(rs.getString("title"));
-          assignment.setContent(rs.getString("content"));
-          assignment.setDeadline(rs.getDate("deadline"));
-          return assignment;
+        } catch (Exception e) {
+            throw new DaoException("데이터 입력 오류", e);
         }
-        return null;
-      }
-
-    } catch (Exception e) {
-      throw new DaoException("데이터 가져오기 오류", e);
     }
-  }
 
-  @Override
-  public int update(Assignment assignment) {
-    try (PreparedStatement pstmt = con.prepareStatement(
-        "update assignments set title=?, content=?, deadline=? where assignment_no=?")) {
+    @Override
+    public int delete(int no) {
+        Connection con = null;
+        try {
+            con = connectionPool.getConnection();
 
-      pstmt.setString(1, assignment.getTitle());
-      pstmt.setString(2, assignment.getContent());
-      pstmt.setDate(3, assignment.getDeadline());
-      pstmt.setInt(4, assignment.getNo());
+            try (PreparedStatement pstmt = con.prepareStatement(
+                "delete from assignments where assignment_no=?")) {
+                pstmt.setInt(1, no);
 
-      return pstmt.executeUpdate();
+                return pstmt.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new DaoException("데이터 삭제 오류", e);
 
-    } catch (Exception e) {
-      throw new DaoException("데이터 변경 오류", e);
+
+        }
     }
-  }
+
+
+    @Override
+    public List<Assignment> findAll() {
+        Connection con = null;
+        try {
+            con = connectionPool.getConnection();
+
+            try (PreparedStatement pstmt = con.prepareStatement(
+                "select assignment_no, title, deadline from assignments order by assignment_no desc");
+                ResultSet rs = pstmt.executeQuery()) {
+
+                ArrayList<Assignment> list = new ArrayList<>();
+
+                while (rs.next()) {
+                    Assignment assignment = new Assignment();
+                    assignment.setNo(rs.getInt("assignment_no"));
+                    assignment.setTitle(rs.getString("title"));
+                    assignment.setDeadline(rs.getDate("deadline"));
+
+                    list.add(assignment);
+                }
+                return list;
+            }
+        } catch (Exception e) {
+            throw new DaoException("데이터 가져오기 오류", e);
+        }
+    }
+
+    @Override
+    public Assignment findBy(int no) {
+        Connection con = null;
+        try {
+            con = connectionPool.getConnection();
+
+            try (PreparedStatement pstmt = con.prepareStatement(
+                "select * from assignments where assignment_no=?")) {
+
+                pstmt.setInt(1, no);
+
+                try (ResultSet rs = pstmt.executeQuery()) {
+
+                    if (rs.next()) {
+                        Assignment assignment = new Assignment();
+                        assignment.setNo(rs.getInt("assignment_no"));
+                        assignment.setTitle(rs.getString("title"));
+                        assignment.setContent(rs.getString("content"));
+                        assignment.setDeadline(rs.getDate("deadline"));
+                        return assignment;
+                    }
+                    return null;
+                }
+            }
+        } catch (Exception e) {
+            throw new DaoException("데이터 가져오기 오류", e);
+        }
+    }
+
+    @Override
+    public int update(Assignment assignment) {
+        Connection con = null;
+        try {
+            con = connectionPool.getConnection();
+
+            try (PreparedStatement pstmt = con.prepareStatement(
+                "update assignments set title=?, content=?, deadline=? where assignment_no=?")) {
+
+                pstmt.setString(1, assignment.getTitle());
+                pstmt.setString(2, assignment.getContent());
+                pstmt.setDate(3, assignment.getDeadline());
+                pstmt.setInt(4, assignment.getNo());
+
+                return pstmt.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new DaoException("데이터 변경 오류", e);
+        }
+    }
 }
