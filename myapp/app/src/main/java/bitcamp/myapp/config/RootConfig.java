f474082369f3e72ab918e183e97679ad65094c1e
심현우 <hyunwoo@simhyeon-uui-MacBookPro.local>
2024-03-27 20:56:37 +0900

    24.03.27

--- myapp/app/src/main/java/bitcamp/myapp/config/RootConfig.java
+++ myapp/app/src/main/java/bitcamp/myapp/config/RootConfig.java
@@ -1,6 +1,5 @@
 package bitcamp.myapp.config;
 
-import javax.sql.DataSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -16,40 +15,45 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
+
 @EnableTransactionManagement
 @MapperScan("bitcamp.myapp.dao")
 @ComponentScan({"bitcamp.myapp.dao", "bitcamp.myapp.service"})
-@PropertySource({"classpath:config/jdbc.properties"})
+@PropertySource({
+        "classpath:config/ncp.properties",
+        "classpath:config/ncp-secret.properties"
+})
 public class RootConfig {
 
-  private final Log log = LogFactory.getLog(this.getClass());
+    private final Log log = LogFactory.getLog(this.getClass());
 
-  public RootConfig() {
-    log.debug("생성자 호출됨!");
-  }
+    public RootConfig() {
+        log.debug("생성자 호출됨!");
+    }
 
-  @Bean
-  public PlatformTransactionManager transactionManager(DataSource dataSource) {
-    return new DataSourceTransactionManager(dataSource);
-  }
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
 
-  @Bean
-  public DataSource dataSource(
-      @Value("${jdbc.url}") String url,
-      @Value("${jdbc.username}") String username,
-      @Value("${jdbc.password}") String password) {
-    return new DriverManagerDataSource(url, username, password);
-  }
+    @Bean
+    public DataSource dataSource(
+            @Value("${jdbc.url}") String url,
+            @Value("${jdbc.username}") String username,
+            @Value("${jdbc.password}") String password) {
+        return new DriverManagerDataSource(url, username, password);
+    }
 
-  @Bean
+    @Bean
 
-  public SqlSessionFactory sqlSessionFactory(ApplicationContext ctx, DataSource dataSource)
-      throws Exception {
-    SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-    sqlSessionFactoryBean.setTypeAliasesPackage("bitcamp.myapp.vo");
-    sqlSessionFactoryBean.setMapperLocations(ctx.getResources("classpath:mapper/*Mapper.xml"));
-    sqlSessionFactoryBean.setDataSource(dataSource);
+    public SqlSessionFactory sqlSessionFactory(ApplicationContext ctx, DataSource dataSource)
+            throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setTypeAliasesPackage("bitcamp.myapp.vo");
+        sqlSessionFactoryBean.setMapperLocations(ctx.getResources("classpath:mapper/*Mapper.xml"));
+        sqlSessionFactoryBean.setDataSource(dataSource);
 
-    return sqlSessionFactoryBean.getObject();
-  }
+        return sqlSessionFactoryBean.getObject();
+    }
 }
--- myapp/app/src/main/java/bitcamp/myapp/service/StorageService.java
+++ myapp/app/src/main/java/bitcamp/myapp/service/StorageService.java
@@ -1,2 +1,10 @@
-package bitcamp.myapp.service;public interface StorageService {
+package bitcamp.myapp.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+
+    String upload(String bucketName, String path, MultipartFile multipartFile) throws Exception;
+
+
 }
