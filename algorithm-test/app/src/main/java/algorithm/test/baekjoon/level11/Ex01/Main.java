2175507e308b6494429adea615a8835156650da7
hyunwoo <kshsim98@naver.com>
2024-08-27 18:48:01 +0900

    0827

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level11/Ex01/Main.java
@@ -0,0 +1,9 @@
+package algorithm.test.baekjoon.level11.Ex01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("0");
+    }
+}
