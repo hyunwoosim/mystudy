394da3c9b38672d72a92074575f2b042847cc246
hyunwoo <kshsim98@naver.com>
2024-08-29 20:07:07 +0900

    0829

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level11/Ex04/Main.java
@@ -0,0 +1,26 @@
+package algorithm.test.baekjoon.level11.Ex04;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    long n = Long.parseLong(br.readLine());
+        br.close();
+
+        bw.write(n*(n-1)/2 + "\n" + 2);
+        bw.flush();
+        bw.close();
+    }
+}
+
+
